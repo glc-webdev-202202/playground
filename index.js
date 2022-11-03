b7ee8ded0923a505e37c56ddf47abe6bd27a753d
Shawn Lee <amarese@snowcorp.com>
2022-11-04 02:03:56 +0900

    add layout

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
+var path = require('path');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.set('view engine', 'pug')
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use("/", indexRouter);
 app.use('/users', usersRouter);
-app.set('view engine', 'pug')
 
 app.listen(8080, () => {
     console.log("server is running!!!!");
